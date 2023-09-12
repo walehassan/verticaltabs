0e86f6dc5e7f70db8abf7d77231381fee68cba9c
walehassan <104022458+walehassan@users.noreply.github.com>
2023-09-12 10:34:40 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const allLinks = document.querySelectorAll('.tabs a');
+const allTabs = document.querySelectorAll('.tab-content')
+const tabContentWrapper = document.querySelector('.tab-content-wrapper');
+
+
+const shiftTabs = (linkId) => {
+    allTabs.forEach((tab, i)) => {
+       
+        if (tab.id.includes(linkId)){
+            
+        }
+    }
+}
